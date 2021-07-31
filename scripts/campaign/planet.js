1f3ad7e54c30d0bc6d6ddef5dcb618d01f044b37
Yagoi23 <82478297+Yagoi23@users.noreply.github.com>
2021-07-31 22:58:52 +1200

    plz work

---
+++ scripts/campaign/planet.js
@@ -0,0 +1,26 @@
+Events.on(ClientLoadEvent, () => {
+
+
+
+    const tontontros = new Planet("tontontros", Planets.serpulo, 1, 0.3);
+    tontontros.generator = new TantrosPlanetGenerator();
+    tontontros.mesh = new HexMesh(tontontros, 5);
+    tontontros.orbitRadius = 3.2;
+    tontontros.orbitTime = 1.5 * 10;
+    tontontros.rotateTime = 30;
+    tontontros.bloom = true;
+    tontontros.accessible = true;
+    tontontros.startSector = 1;
+    tontontros.alwaysUnlocked = true;
+    tontontros.localizedName = "Moon";
+
+
+
+    const antarcticaSurvival = new SectorPreset("fork", tontontros, 1);
+    antarcticaSurvival.captureWave = 30;
+    antarcticaSurvival.localizedName = "Fork";
+    antarcticaSurvival.difficulty = 6;
+    antarcticaSurvival.alwaysUnlocked = true;
+
+
+});
--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,18 @@
 
 
-require("campaign/mun");
+require("campaign/planet");
 
 print("Load Complete!");
+
+
+
+
+
+
+/*
+credit(){
+  younggam: "answering tons of question",
+  deltaNedas: "refer to routorio, rtfm",
+  sk7725: "answering tons of question, refer to testers"
+}
+*/
