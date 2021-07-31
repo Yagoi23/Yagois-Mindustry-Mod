653830fe2ce1a8c2d5940f161196331bdcf39c31
Yagoi23 <82478297+Yagoi23@users.noreply.github.com>
2021-07-31 16:24:36 +1200

    added rogerla

---
+++ scripts/rogerla.js
@@ -0,0 +1,26 @@
+// Just a little note to not forget Pixellandia :D
+//I won't forget :)
+const rogerla = extend(Planet, "rogerla", Planets.sun, 1, 0.5, {
+    generator: new SerpuloPlanetGenerator(),
+    bloom: true,
+    radius: 1,
+    accessible: true,
+    hasAtmosphere: true,
+    atmosphereColor: Color.valueOf("80ff00"),
+    atmosphereRadIn: 0.02,
+    atmosphereRadOut: 0.3,
+    localizedName: "Rogerla"
+});
+rogerla.meshLoader = () => extend(HexMesh, rogerla, 6, {});
+
+const lavaflow = extend(SectorPreset, "lavaflow", rogerla, 6, {
+    captureWave: 14,
+    localizedName: "Lava Flow",
+    difficulty: 1,
+    alwaysUnlocked: true
+});
+
+module.exports = {
+    rogerla: rogerla,
+    lavaflow: lavaflow,
+}
